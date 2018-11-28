d1ee6262a285b01066d0fb77fb17f1dd3ff4a877
Firestar99 <31222740+firestar99@users.noreply.github.com>
2018-11-28 13:33:38 +0100

    added wrapping html

--- src/node.js
+++ src/node.js
@@ -1,10 +1,33 @@
 var http = require('http');
+var fs = require('fs');
 
+
+//utility functions
+
+
+//load resources
+console.log("loading resources...");
+
+var wrapping;
+fs.readFile(__dirname + '/Wrapping.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    wrapping = data;
+});
+
+console.log("resources loaded!");
+
+
+//start http server
 console.log("starting http server...");
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.write(wrapping);
+
+    res.end();
 }).listen(8080);
 
 console.log("http server started!");
