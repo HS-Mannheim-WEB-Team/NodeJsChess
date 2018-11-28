3a74d5255736ca4559199f6411f6016277138df4
Firestar99 <31222740+firestar99@users.noreply.github.com>
2018-11-28 14:55:16 +0100

    socket.io working

--- src/node.js
+++ src/node.js
@@ -1,33 +1,24 @@
-var http = require('http');
+var http = require('http').createServer(handlehttp);
+var io = require('socket.io')(http);
 var fs = require('fs');
 
-
-//utility functions
+http.listen(8080);
 
 
 //load resources
-console.log("loading resources...");
-
-var wrapping;
-fs.readFile(__dirname + '/Wrapping.html', function (err, data) {
-    if (err) {
-        throw err;
-    }
-    wrapping = data;
-});
-
-console.log("resources loaded!");
+var file_socket_io_js = "<script>\n" + fs.readFileSync(__dirname + '/include/socket.io.js').toString() + "\n</script>";
+var file_wrapping = fs.readFileSync(__dirname + '/Chess.html').toString();
 
 
-//start http server
-console.log("starting http server...");
-
-http.createServer(function (req, res) {
+//http server
+function handlehttp(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    res.write(wrapping);
+    res.write(file_wrapping.replace("<chessheader/>", file_socket_io_js));
 
     res.end();
-}).listen(8080);
+}
 
-console.log("http server started!");
+io.on('connection', function (socket) {
+    console.log('a user connected');
+});
