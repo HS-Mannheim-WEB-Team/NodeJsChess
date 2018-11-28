cfabc02c15f419491612f65b69c25f22ec0fee95
Firestar99 <31222740+firestar99@users.noreply.github.com>
2018-11-28 15:18:15 +0100

    better external file including

--- src/node.js
+++ src/node.js
@@ -6,15 +6,17 @@ http.listen(8080);
 
 
 //load resources
-var file_socket_io_js = "<script>\n" + fs.readFileSync(__dirname + '/include/socket.io.js').toString() + "\n</script>";
-var file_wrapping = fs.readFileSync(__dirname + '/Chess.html').toString();
+var file_index = fs.readFileSync(__dirname + '/Chess.html').toString();
+file_index = file_index.replace(/<script include="(.+?)"><\/script>/gi, function (match, p1) {
+    return "<script>\n" + fs.readFileSync(__dirname + p1).toString() + "\n</script>";
+});
 
 
 //http server
 function handlehttp(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
 
-    res.write(file_wrapping.replace("<chessheader/>", file_socket_io_js));
+    res.write(file_index);
 
     res.end();
 }
