52a6a1ca750a1973bcb190efee05b836b9ef546c
Firestar99 <31222740+firestar99@users.noreply.github.com>
2018-11-29 11:13:26 +0100

    moved css and js into separate files

---
+++ src/socketIoClient.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    //socket.io connection
+    const socket = io.connect('http://localhost:8080');
+    socket.on('connect', function () {
+        console.log("client: connected");
+    });
+
+    //rebuild chessfield
+    socket.on('chessfield', function (figures) {
+        let htmlout = "\n";
+        for (let y = 0; y < 10; y++) {
+            htmlout += "<tr>";
+            for (let x = 0; x < 10; x++) {
+                const fieldBlack = (x + y) % 2 === 0 ? "field-black" : "field-white";
+                const borderLetter = x === 0 || x === 9;
+                const borderNumber = y === 0 || y === 9;
+                if (borderLetter && borderNumber) {
+                    htmlout += `<td class="${fieldBlack} field-border"/>`;
+                } else if (borderLetter) {
+                    htmlout += `<td class="${fieldBlack} field-border">${String.fromCharCode('8'.charCodeAt(0) - y + 1)}</td>`;
+                } else if (borderNumber) {
+                    htmlout += `<td class="${fieldBlack} field-border">${String.fromCharCode('A'.charCodeAt(0) + x - 1)}</td>`;
+                } else {
+                    htmlout += `<td id="field-id-${y}-${x}" class="${fieldBlack} field-inner${figures[y - 1][x - 1] ? " " + figures[y - 1][x - 1].toString() : ""}"/>`;
+                }
+            }
+            htmlout += "</tr>\n";
+        }
+        $("#chessfield").html(htmlout);
+    });
+});
